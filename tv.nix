c8bfb404d7e7f4bcc2195e29633abb76e9703186
Nejc Zupan <nejc.zupan@gmail.com>
2018-12-28 00:26:19 +0100

    networking for tv.nix; configuration.nix is no longer needed

--- tv.nix
+++ tv.nix
@@ -22,4 +22,25 @@
 
   swapDevices = [ { label = "swap"; }];
 
+  networking = {
+    hostName = "tv";
+    interfaces.eth0.ipv4.addresses = [{
+        address = "10.9.3.10";
+        prefixLength = 24;
+    }];
+    defaultGateway = "10.9.3.1";
+    nameservers = [ "10.9.3.1" ];
+ };
+
+  fileSystems."/mnt/nas" = {
+      device = "//NAS/media";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    };
+
+
 }
