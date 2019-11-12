46a59c40815389f47f34724450d4f912c93e0ecf
Nejc Zupan <nejc.zupan@gmail.com>
2019-11-12 21:50:51 +0000

    Add docs how to prepare the Vultr VPS

--- vultr.nix
+++ vultr.nix
@@ -1,3 +1,27 @@
+# A Vultr VPS that I use to play/learn with NixOS
+#
+# Usage:
+# - Create new Vultr instance from NixOS 18.09 ISO
+# - Click `View Console`
+#   $ curl https://eipi.xyz/vultr.sh | bash
+# - Click Settings -> Custom ISO -> Remove ISO
+# $ ssh root@<IP>  # password is "qqcTch4m"
+#   $ vim /etc/nixos/configuration.nix  # add git to environment.systemPackages
+#   $ nixos-rebuild switch
+#   $ cd /etc/nixos
+#     $ git init
+#     $ git remote add origin https://github.com/zupo/nix.git
+#     $ git pull
+#     $ git checkout master
+#     $ mv configuration.nix configuration.nix.bak
+#     $ mv hardware-configuration.nix vultr/hardware-configuration.nix
+#     $ ln -s vultr.nix configuration.nix
+#     $ passwd  # set a new root password
+#     $ nixos-rebuild switch
+# $ ssh zupo@<IP>  # no password should be required, login with pubkey
+#   $ sudo su -
+#   $ reboot
+
 { config, pkgs, ... }:
 
 {
